5e64d333eff6c7cf4edd22d3b197cd5359bc717c
Fenrir <94824143+Fenrir0279@users.noreply.github.com>
2022-12-21 17:08:02 +0530

    Update 100_doors.py

--- 100_doors.py
+++ 100_doors.py
@@ -1,11 +1,16 @@
 # https://rosettacode.org/wiki/100_doors
+ 
+def main():
+    doors = [False]*100
+    for n in range(1, 101):
+        for index, door in enumerate(doors):
+            if index % n == 0:
+                doors[index] = not door
 
-doors = [False]*100
-for n in range(1, 101):
-    for index, door in enumerate(doors):
-        if index % n == 0:
-            doors[index] = not door
+    for i, door in enumerate(doors):
+        if door: print(i, end=' ')
+    print()
 
-for i, door in enumerate(doors):
-    if door: print(i, end=' ')
-print()
+if __name__ == "__main__":
+    main()
+    
