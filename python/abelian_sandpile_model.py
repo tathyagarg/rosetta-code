4526caf283dedfa1976f450832f5100d8c929f2e
Fenrir <94824143+Fenrir0279@users.noreply.github.com>
2022-12-26 23:06:19 +0530

    Update abelian_sandpile_model.py

--- python/abelian_sandpile_model.py
+++ python/abelian_sandpile_model.py
@@ -46,8 +46,10 @@ class AbelianModel:
         f = f.convert('RGB').resize((len(self.pile)*8,)*2, resample=Image.BOX)
         f.save(name)
 def main():
-    AbelianModel(sand_pile).make_image('before.png')
-    AbelianModel(sand_pile).collapse().make_image('after.png')
+    model = AbelianModel(sand_pile)
+    
+    model.make_image('before.png')
+    model.collapse().make_image('after.png')
 
 if __name__ == "__main__""
     main()
